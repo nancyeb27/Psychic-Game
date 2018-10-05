ed9ebfc748cd3ae07f7e4c947d68eb1b16cb66c5
Nancyb <nancyeb27@yahoo.com>
2018-10-04 22:34:02 -0600

    second draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,57 @@
+// ESTABLISH var for user and computer 
 
+var computerChoices = ["a","b","c","x","y","z"]
 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var totalGuesses = 0;
+var computerGuess = "";
+var userGuess = "";
 
+document.onkeyup = function (event) {
+       userGuess = event.key;
+       computerGuess = computerChoices[Math.floor(Math.random( Math.floor) * computerChoices
+            .length)];
+      console.log( "userguess",event.key);
+      console.log("computerGuess",computerGuess);
+      // console.log(guessedLetters);
 
-
-document.onkeyup = function () {
-    
-var computerChoices = ["a","b","c","x","y","x"]
-var userGuess = String.fromCharCode(event.keyCode).
-        toLowerCase();
-
-var wins = "0";
-var losses = "0";
-var guessesLeft = "9";
-var totalGuesses = "0";
-
-console.log(userGuess)
-
-//computer random choice
-var computerGuess = computerChoices [Math.floor(Math.random( Math.floor) * computerChoices
-    .length)];
-
-    console.log(computerGuess);
-    
-  if (userGuess === computerGuess) {
+      if ((userGuess === computerGuess) && (guessesLeft > 0)) {
+                    
         wins++; 
-        alert ("Wins: " + wins);  
-        alert ("You Win!");
+      //   guessesLeft = 9;
+      //   totalGuesses.length = 0;
+      //   computerGuess.length = 0;
+         
+      console.log(computerGuess);
+            alert ("Wins: " + wins);  
+            alert ("You Win!");
 
   }
-    else {
-               
-        alert ("Please choose agian.");
-        losses++;
-        guessesLeft--;
-        totalGuesses++;
-        alert ("Losses: " + losses);
-        alert ("Guess Remaining: " + guessesLeft);
-        alert ("Total Guesses: " + totalGuesses++);
-}
+    else if ((userGuess !== computerGuess) && (guessesLeft < 0)) {
+               guessesLeft--;
+               alert ("No more guess left - try truck driving for a career.")
+    }
+      else {
+            losses++; 
+        guessesLeft = 9;
+      //   totalGuesses.length = 0;
+      //   computerGuess.length =0;
+        
+        computerGuess = computerChoices[Math.floor(Math.random( Math.floor) * computerChoices
+            .length)];
+
+            console.log(computerGuess);
+            
+
+      }
+}        
     
-
-var html = "Guess what letter I am thinking of  " +
-     "wins " + wins + "Losses " + losses + "Total Guesses " + totalGuesses + "Guess Remaining"
-      + guessesLeft;
-
-
   
-document.querySelector("#guess").innerHTML = html;
+// document.querySelector("#game").innerHTML = html;
 
-}
+
 
 
 
